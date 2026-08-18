e5decc9c63cad475739d280464a549756da702e3
Admin <dmadhusudhan98@gmail.com>
2026-08-18 18:41:16 +0530

    added

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import {
   Mandali,
   Ramabhadra,
 } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import ConditionalLayout from "@/components/ConditionalLayout";
 import LucidFocusTracker from "@/components/LucidFocusTracker";
@@ -106,7 +107,45 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en-IN" className={`${manrope.variable} ${nunitoSans.variable} ${sourceSerif4.variable} ${mandali.variable} ${ramabhadra.variable} h-full antialiased`}>
+      
+      {/* Google Ads */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-18396742379"
+        strategy="afterInteractive"
+      />
+      <Script id="google-ads" strategy="afterInteractive">{`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'AW-18396742379');
+      `}</Script>
+
+      {/* Meta Pixel */}
+      <Script id="meta-pixel" strategy="afterInteractive">{`
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1093634546952615');
+        fbq('track', 'PageView');
+      `}</Script>
+
       <body className="min-h-full flex flex-col bg-cream font-sans">
+        {/* Meta Pixel noscript fallback */}
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1093634546952615&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <ConditionalLayout>{children}</ConditionalLayout>
         <LucidFocusTracker />
       </body>
